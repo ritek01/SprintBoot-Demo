788e6807daf712eaf684cebb280e4a2794245d06
Ritek <ritek.rounak@harness.io>
2024-05-27 18:28:11 +0530

    :tada: Demo Project

--- src/main/java/com/hackweek/spring_boot/Application.java
+++ src/main/java/com/hackweek/spring_boot/Application.java
@@ -2,9 +2,18 @@ package com.hackweek.spring_boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Application {
+
+	@RequestMapping("/")
+    public String home() {
+		// H1 heading
+        return "<h1>Welcome to Spring Boot</h1>";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
